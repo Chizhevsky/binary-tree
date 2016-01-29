bd89b56f141232d747e42c588b783d2039ee09c7
Chizhevsky <alexvinsent96@yandex.ru>
2016-01-29 21:15:36 +0300

    plus containes

--- binary-tree.js
+++ binary-tree.js
@@ -1,7 +1,6 @@
 'use strict';
 
 class BinaryTree {
-
 	constructor() {
 		this.root = null;
 	}
@@ -10,45 +9,54 @@ class BinaryTree {
 		var node = new Node(data, null, null);
 
     var current;
-    if (this.root === null){
-                this.root = node;
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      current = this.root;
+      while(true) {
+        //if the new value is less than this node's value, go left
+        if (data < current.data) {
+            //if there's no left, then the new node belongs there
+            if (current.left === null) {
+                current.left = node;
+                break;
             } else {
-              current = this.root;
-              while(true){
+                current = current.left;
+        }
 
-                //if the new value is less than this node's value, go left
-                if (data < current.data){
+        //if the new value is greater than this node's value, go right
+      } else if (data > current.data) {
 
-                    //if there's no left, then the new node belongs there
-                    if (current.left === null){
-                        current.left = node;
-                        break;
-                    } else {
-                        current = current.left;
-                    }
-
-                //if the new value is greater than this node's value, go right
-              } else if (data > current.data){
-
-                    //if there's no right, then the new node belongs there
-                    if (current.right === null){
-                        current.right = node;
-                        break;
-                    } else {
-                        current = current.right;
-                    }
-
-                //if the new value is equal to the current one, just ignore
-                } else {
-                    break;
-                }
+            //if there's no right, then the new node belongs there
+            if (current.right === null) {
+                current.right = node;
+                break;
+            } else {
+                current = current.right;
             }
+
+        //if the new value is equal to the current one, just ignore
+        } else {
+            break;
+        }
+    }
 	}
 }
 
 
 	contains(data) {
-
+		var current = this.root,
+			found = false;
+		while (!found && current) {
+			if (data < current.data) {
+				current = current.left;
+			} else if (data > current.data) {
+				current = current.right;
+			} else {
+				found = true;
+			}
+		}
+		return found;
 	}
 
 	remove(data) {
